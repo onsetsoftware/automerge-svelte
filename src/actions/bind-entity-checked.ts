e496924577d9dacbdaa78629b8240b36494df0f6
Alex Currie-Clark <acurrieclark@gmail.com>
2023-05-24 09:10:25 +0100

    add bindEntityChecked action

---
+++ src/actions/bind-entity-checked.ts
@@ -0,0 +1,37 @@
+import { inputAction } from "./input-action";
+import { BindEntityOptions } from "./types/bind-entity-options.type";
+
+export function bindEntityChecked<
+  U,
+  T extends { id: string; [key: string]: any },
+>(node: HTMLInputElement, options: BindEntityOptions<U, T>) {
+  return inputAction(
+    {
+      subscribe: (node, { store, property, ids }) => {
+        return store.subscribe((doc) => {
+          const values = new Set(ids.map((id) => doc.entities[id]?.[property]));
+
+          if (values.size === 1) {
+            node.checked = Boolean(doc.entities[ids[0]]?.[property]);
+          } else {
+            node.checked = false;
+            node.indeterminate = true;
+          }
+        });
+      },
+      inputListener: (node, { store, ids, property, title }) => {
+        store.change(
+          (doc) => {
+            ids.forEach((id) => {
+              // @ts-ignore
+              doc.entities[id][property] = node.checked;
+            });
+          },
+          title ? { message: `Update ${title}` } : {},
+        );
+      },
+    },
+    node,
+    options,
+  );
+}
--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ export { bindString } from "./actions/bind-string";
 export { bindStringDeferred } from "./actions/bind-string-deferred";
 export { bindEntityStringDeferred } from "./actions/bind-entity-string-deferred";
 export { bindEntityValueDeferred } from "./actions/bind-entity-value-deferred";
+export { bindEntityChecked } from "./actions/bind-entity-checked";
 
 export { AutomergeSvelteStore } from "./automerge-svelte-store";
 export { AutomergeDerivedStore } from "./automerge-derived-store";
